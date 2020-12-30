d5a04f45ffcf5307a8faf5fdf75781864311313d
Chris Blaise <cblaise@untangle.com>
2020-12-30 09:28:30 -0500

    NGFW-12764 Add column to show percent of ssytem rss that the process memory is using

--- support_diagnostics/platforms/all/analyzers/process_memory.py
+++ support_diagnostics/platforms/all/analyzers/process_memory.py
@@ -20,18 +20,24 @@ class ProcessMemoryAnalyzer(Analyzer):
     max_records = 10
     heading = "Top 10 Process Memory Usage"
     categories = ["os"]
-    collector = ProcessesCollector
+    collector = [ ProcessesCollector, MemoryCollector]
 
     def analyze(self, collector_results):
         results = []
 
+        memory_total = None
+        for result in filter(lambda r: r.source == "memory", collector_results):
+            if "memtotal" in result.output:
+                memory_total = result.output["memtotal"]
+
         memory_sorted_process_results = sorted(filter(lambda r: r.source == "process" and 'vmrss' in r.output['status'], collector_results), key=lambda d: d.output['status']['vmrss'], reverse=True)
         for process_result in memory_sorted_process_results[:ProcessMemoryAnalyzer.max_records]:
-            result = AnalyzerResult(severity=AnalyzerResultSeverityPass,other_results={ "{severity}" : '{cmdline:<55} {size}'})
+            result = AnalyzerResult(severity=AnalyzerResultSeverityPass,other_results={ "{severity}" : '{cmdline:<55} {size:<11} {percent:<.2f}%'})
             cmdline_len = len(process_result.output['cmdline'])
             format_fields = {
                 'cmdline': '{process}{elide}'.format(process=process_result.output['cmdline'][:50], elide='...' if cmdline_len > 50 else ''),
-                'size': support_diagnostics.utilities.SizeConversion.to_human(process_result.output['status']['vmrss'])
+                'size': support_diagnostics.utilities.SizeConversion.to_human(process_result.output['status']['vmrss']),
+                'percent': round(process_result.output['status']['vmrss'] / memory_total * 100, 2)
             }
             result.collector_result = process_result
             result.analyzer = self
