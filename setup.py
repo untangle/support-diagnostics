d60c77af1d73796c625b27d5caccaf490b3f6fe0
Chris Blaise <cblaise@untangle.com>
2021-10-01 09:47:04 -0400

    Add support to build.

---
+++ setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+"""setup.py"""
+
+import sys
+
+from setuptools import setup
+from subprocess import check_output
+from os.path import isdir
+
+
+if isdir("../.git") or isdir(".git"): # debian source tarballs don't contain .git
+    version_cmd = "git describe --tags --always --long"
+    version = check_output(version_cmd.split(" ")).decode().strip()
+    print("Working on git version {}".format(version))
+    # enforce https://www.python.org/dev/peps/pep-0440
+    items = version[1:].split('-')
+    if len(items) == 3:
+        version = '{}+{}'.format(items[0], items[2][1:])
+    print("--> PEP-0440 version will be {}".format(version))
+    with open('support_diagnostics/version.py', 'w') as f:
+        f.write('__version__ = "{}"\n'.format(version))
+else:
+    version = "undefined"
+
+if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
+    pytestRunner = ['pytest-runner']
+else:
+    pytestRunner = []
+
+setup(name='support-dignostics',
+      version=version,
+      description='Support Diagnostics.',
+      long_description='''Analyze live Untangle system and produce report.''',
+      author='Untangle.',
+      author_email='cblaise@untangle.com',
+      url='https://untangle.com',
+      scripts=['bin/support_diagnostics'],
+      packages=['support_diagnostics'],
+      install_requires=['urllib3'],
+      license='GPL',
+      setup_requires=pytestRunner,
+      tests_require=[
+        "pytest",
+        "pytest-cov"
+      ],
+      #      test_suite='',
+      #      cmdclass={'test': PyTest},
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'License :: OSI Approved :: General Public License v2 (GPL-2)',
+          'Environment :: Console',
+          'Operating System :: POSIX',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9'
+      ])
---
+++ tests/test_support_diagnostics.py
@@ -0,0 +1,8 @@
+import subprocess
+import unittest
+import json
+import sys
+import traceback
+
+def test():
+    assert(True is True)
